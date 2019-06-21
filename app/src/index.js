2a991ac8650bc718bf881f0e1faf597760badfbf
Alex Franz <sansan@kashenka.ru>
2019-06-22 00:19:26 +0300

    [tech-task] moment was added, LocalProvider set

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 
+import { LocaleProvider } from 'antd';
+import moment from 'moment';
+import ruRu from 'antd/lib/locale-provider/ru_RU';
+import 'moment/locale/ru';
+
 import configureStore, { history } from './stores/configureStore';
 import App from './components/App';
 
@@ -10,12 +15,16 @@ import '../styles/index.scss';
 
 const store = configureStore();
 
+moment.locale('ru');
+
 function render (Component) {
   ReactDOM.render(
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Component/>
-      </ConnectedRouter>
+      <LocaleProvider locale={ruRu}>
+        <ConnectedRouter history={history}>
+          <Component/>
+        </ConnectedRouter>
+      </LocaleProvider>
     </Provider>,
     document.getElementById('app')
   );
