db3c2bb22136572340ed7c2de85db483efd4872b
Alex Franz <sansan@kashenka.ru>
2019-07-24 19:45:05 +0300

    * body-parser was added to dev-mock-server * createPageablePostRoute method was added to routes in dev-mock-server

--- mock-server/server.js
+++ mock-server/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const port = 3003;
 require('./app/routes')(app);
