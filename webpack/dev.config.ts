a26b5a7a257d4941cd1d94515e66a8b4944f1f91
Alex Franz <ut1339@gmail.com>
2021-06-20 01:58:48 +0300

    webpack@5 migration

--- webpack/dev.config.ts
+++ webpack/dev.config.ts
@@ -1,68 +1,162 @@
-import { Configuration } from 'webpack';
-
+const path = require('path');
 const webpack = require('webpack');
-const { merge } = require('webpack-merge');
 
 /* VERSION (from git tags), BRANCH and COMMIT to files header */
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const baseConfig = require('./base.config');
+const loaders = require('./loaders');
 const settings: { resourcePrefix: string, htmlPlugin: Record<string, unknown> } = require('./settings');
 
-const devConfig = (): Configuration => merge([
-  {
-    mode: 'development',
-    output: {
-      filename: `./js/[name].[chunkhash]${settings.resourcePrefix}.js`
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        __DEBUG__: JSON.stringify(true),
-        __TEST__: JSON.stringify(false),
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        SERVER_MODULE_NAME: JSON.stringify('new-begin'),
-        SERVER_PATH: JSON.stringify('../api')
-      }),
-      new MiniCssExtractPlugin({
-        filename: `./css/[name].[contenthash]${settings.resourcePrefix}.css`,
-        chunkFilename: `./css/[id]${settings.resourcePrefix}.css`,
-        allChunks: true
-      })
-    ],
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: 'styles',
-            test: /\.css$/,
-            chunks: 'all',
-            enforce: true
+module.exports = {
+  context: settings.rootDir,
+  entry: {
+    app: path.resolve(settings.rootDir, 'app', 'src', 'index.tsx')
+  },
+  mode: 'development',
+  output: {
+    filename: `./js/[name].[chunkhash]${settings.resourcePrefix}.js`
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          loaders.getCacheLoader(path.resolve(settings.cacheDir, 'css')),
+          loaders.getThreadLoader('css'),
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          loaders.getCacheLoader(path.resolve(settings.cacheDir, 'css')),
+          loaders.getThreadLoader('css'),
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
           }
+        ]
+      },
+      {
+        test: /\.(png|gif|jpe?g)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[contenthash].[ext]'
         }
+      },
+      {
+        test: /\.(svg|woff|woff2|eot|ttf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: `fonts/[contenthash].[ext]?${Date.now()}`
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        use: ['source-map-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        use: ['eslint-loader'],
+        exclude: /(node_modules)/
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          loaders.getCacheLoader(path.resolve(settings.cacheDir, 'js')),
+          loaders.getThreadLoader('js'),
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: path.resolve(settings.cacheDir, 'babel')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  externals: {},
+  target: 'web',
+  resolve: {
+    alias: settings.aliases,
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new AntdDayjsWebpackPlugin(),
+    new HtmlWebpackPlugin(settings.htmlPlugin),
+    new webpack.DefinePlugin({
+      __DEBUG__: JSON.stringify(true),
+      __TEST__: JSON.stringify(false),
+      SERVER_MODULE_NAME: JSON.stringify('new-begin'),
+      SERVER_PATH: JSON.stringify('../api')
+    }),
+    new MiniCssExtractPlugin({
+      filename: `./css/[name].[contenthash]${settings.resourcePrefix}.css`,
+      chunkFilename: `./css/[id]${settings.resourcePrefix}.css`,
+      allChunks: true
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          enforce: true
+        },
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
+        }
+      },
+      chunks: 'all'
+    }
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    clientLogLevel: 'info',
+    compress: false,
+    contentBase: 'dist',
+    historyApiFallback: {
+      index: 'index.html'
+    },
+    open: false,
+    port: 9090,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3003',
+        secure: false
       }
     },
-    devtool: 'inline-source-map',
-    devServer: {
-      clientLogLevel: 'info',
-      compress: false,
-      contentBase: 'dist',
-      historyApiFallback: {
-        index: 'index.html'
-      },
-      open: false,
-      port: 9090,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:3003',
-          secure: false
-        }
-      },
-      stats: {
-        colors: true
-      },
-      watchContentBase: true
-    }
+    stats: {
+      colors: true
+    },
+    watchContentBase: true
   }
-]);
-
-module.exports = () => merge(baseConfig(), devConfig());
+};
